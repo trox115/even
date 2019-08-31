c8898673318b151064db17295b8e3ec4c365f022
trox115 <tofernandes67@gmail.com>
2019-08-31 03:22:32 +0100

    added styling to pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-  get 'static_pages/contact'
+  get  '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
   resources :users
   root 'static_pages#home'
 end
