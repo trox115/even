a7b7ca25931a46e94999d82125b8ab36cfcdac6b
trox115 <tofernandes67@gmail.com>
2019-08-31 04:52:13 +0100

    users can register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,26 @@ class UsersController < ApplicationController
   end
 
   def show
-  
+    @user = User.find(params[:id])
   end
 
+  def new
+    @user=User.new
+  end
 
+  def create
+    @user=User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Events App, #{@user.name}!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+  params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
