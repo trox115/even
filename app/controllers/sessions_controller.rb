fe12e32ae7ca50736ff2acf474041836e915205a
trox115 <tofernandes67@gmail.com>
2019-09-05 23:45:26 +0100

    User can login throug name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(name: params[:session][:name])
+    if user
+      log_in user
+      redirect_to user
+
+    else
+      flash.now[:danger] = 'This user does not exist!'
+      render 'new'
+    end
   end
+
+  def destroy; end
+
+  private
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   get 'static_pages/home'
   get  '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
   root 'static_pages#home'
 end
